d70bdc3d3f2bf2aadf75badf0ef0c5cd9202a4e2
lingjian99 <lingjian99@yeah.net>
2022-06-14 15:52:45 -0700

    midified images virtual path

--- src/main/java/com/gordon/dosaic/WebMvcConfig.java
+++ src/main/java/com/gordon/dosaic/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer{
  
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/photo/**").addResourceLocations("file:c:/home/images/");
+        registry.addResourceHandler("/photo/**").addResourceLocations("file:/home/images/");
     }
 
 }
